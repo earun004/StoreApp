1663a0df38863e3ab4169f6904f1bb00e56eaf6e
mrudula666 <nmrchandra3@gmail.com>
2024-01-06 18:51:33 -0800

    added AdminSecurity Config for Aws Access key.

--- src/main/java/com/kosuri/stores/config/AWSConfig.java
+++ src/main/java/com/kosuri/stores/config/AWSConfig.java
@@ -1,5 +1,8 @@
 package com.kosuri.stores.config;
 
+import com.kosuri.stores.dao.AdminSecurityEntity;
+import com.kosuri.stores.dao.AdminSecurityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,13 +11,16 @@ import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
+import java.util.Optional;
+
 @Configuration
 public class AWSConfig {
 
-    @Value("${aws.access-key}")
+    @Autowired
+    private AdminSecurityRepository adminSecurityRepository;
+
     private String awsAccessKey;
 
-    @Value("${aws.secret-key}")
     private String awsSecretKey;
 
     @Value("${aws.region}")
@@ -22,6 +28,13 @@ public class AWSConfig {
 
     @Bean
     public S3Client s3Client() {
+        Optional<AdminSecurityEntity> adminSecurityOptional = adminSecurityRepository.findById(1);
+        if (adminSecurityOptional.isPresent()){
+            AdminSecurityEntity adminSecurity = adminSecurityOptional.get();
+             awsAccessKey = adminSecurity.getAwsAccessKey();
+             awsSecretKey = adminSecurity.getAwsSecretKey();
+        }
+
         AwsBasicCredentials awsCreds = AwsBasicCredentials.create(awsAccessKey, awsSecretKey);
 
         return S3Client.builder()
